1f8614b13bf434e35183156be721961aae631ae0
Quentin Sahal <quentin.sahal@gmail.com>
2017-02-21 10:18:56 +0100

    Add files via upload

--- gensignal.c
+++ gensignal.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <libproc.h>
+#include <sys/resource.h>
 #include <sys/time.h>
 #include <time.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdlib.h>
 #include <string.h>
 #include <mach/mach_time.h>
 
@@ -21,12 +23,18 @@ struct timespec clock_gettime(void) {
   return t;
 }
 
+void handler_signal(int);
+
 
 double diffclock(struct timespec a,struct timespec b)
 {
 	return ((double)b.tv_sec + ORWL_NANO*b.tv_nsec - (double)a.tv_sec + ORWL_NANO*a.tv_nsec);
 
 }
+
+int hasToSleep  = -1;
+int nb_of_timer = 0;
+int timeToSleep = 0;
 
 int main(int argc, char const *argv[])
 {
@@ -48,44 +56,122 @@ int main(int argc, char const *argv[])
 
 	// }
 	// le temps sera exprimé en micro sec
-	double temps_actif, temps_sleep;
-	if(argc == 5 )
-	{
-		printf("%s\n",argv[2] );
-		if(strcmp(argv[1], "-a") == 0)
-		{
-			temps_actif = atof(argv[2]);
-		}
-		if(strcmp(argv[3], "-s") == 0)
-		{
-			temps_sleep = atof(argv[4]);
-		}
-	}
-	else 
-	{
-		printf("Le nombre d'argument n'est pas correct\n");
-		exit(1);
-	}
-	printf("%f, %f\n",temps_sleep, temps_actif );
+
+/* Gere les parametres en entrée
+
+	// double temps_actif, temps_sleep;
+	// if(argc == 5 )
+	// {
+	// 	if(strcmp(argv[1], "-a") == 0)
+	// 	{
+	// 		temps_actif = atof(argv[2]);
+	// 	}
+	// 	if(strcmp(argv[3], "-s") == 0)
+	// 	{
+	// 		temps_sleep = atof(argv[4]);
+	// 	}
+	// }
+	// else 
+	// {
+	// 	printf("Le nombre d'argument n'est pas correct\n");
+	// 	exit(1);
+	// }
+
+*/
+
+	//printf("%f, %f\n",temps_sleep, temps_actif );
+
+	// while(1)
+	// {
+	// 	clock_t tstart, tend;
+	// 	tstart = tend = clock();
+	// 	//printf("on démarre\n\n");
+	// 	while( (double)(tend - tstart)/CLOCKS_PER_SEC <= temps_actif/1000000.0 ){
+	// 		//printf("%f\n",(double)(tend - tstart) );
+	// 		//printf("%f\n",diffclock(tstart, tend)/1000);
+	// 		//printf("difftime: %f\n",difftime(end_t, start_t));
+	// 		tend = clock();
+	// 		//printf("%ld, %ld, %f \n",start_t, end_t, difftime(end_t, start_t)/1000.0 );
+	// 	}
+	// 	//printf("fin de la boucle while\n");
+	// 	usleep(temps_sleep);
+	// 	//printf("fin du sleep\n");
+
+	// }
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+	struct itimerval timer;
+	signal(SIGALRM, handler_signal);
+
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_usec = 100;
+
+	timer.it_value = timer.it_interval ; 
+
+	setitimer(ITIMER_REAL, &timer, NULL);
 
 	while(1)
 	{
-		clock_t tstart, tend;
-		tstart = tend = clock();
-		//printf("on démarre\n\n");
-		while( (double)(tend - tstart)/CLOCKS_PER_SEC <= temps_actif/1000000.0 ){
-			//printf("%f\n",(double)(tend - tstart) );
-			//printf("%f\n",diffclock(tstart, tend)/1000);
-			//printf("difftime: %f\n",difftime(end_t, start_t));
-			tend = clock();
-			//printf("%ld, %ld, %f \n",start_t, end_t, difftime(end_t, start_t)/1000.0 );
-		}
-		//printf("fin de la boucle while\n");
-		usleep(temps_sleep);
-		//printf("fin du sleep\n");
-
+		
+		if(hasToSleep ){
+			hasToSleep = -1;
+			usleep(timeToSleep);
+		}	
 	}
 	
 
 	return 0;
+}
+
+
+
+void handler_signal(int inutilisee)
+{
+	//struct proc_taskallinfo tiDepart, tiFin;
+	long tDepart,tFin;
+	struct rusage r;
+	long totalUserTime, totalSystemTime;
+
+	switch(nb_of_timer%2)
+	{
+		case 0: //on a passé les 500ms
+			
+			hasToSleep = -1;
+			//proc_pidinfo(getpid(), PROC_PIDTASKALLINFO, 0, &tiDepart, sizeof(tiDepart));
+			getrusage(RUSAGE_SELF, &r);
+			tDepart = 1000000*r.ru_utime.tv_sec +  r.ru_utime.tv_usec + 1000000*r.ru_stime.tv_sec +  r.ru_stime.tv_usec;
+
+			//fprintf(stderr, "on remet le nb de timer à :%d\n", nb_of_timer);
+			break;
+		case 1 : // On est au bout de 100ms
+			//fprintf(stderr, "nb timer : %d\n", nb_of_timer);
+			hasToSleep = 1 ;
+			// proc_pidinfo(getpid(), PROC_PIDTASKALLINFO, 0, &tiFin, sizeof(tiFin)); 
+			// totalUserTime = (tiFin.ptinfo.pti_total_user  - tiDepart.ptinfo.pti_total_user ); 
+			// totalSystemTime = (tiFin.ptinfo.pti_total_system  - tiDepart.ptinfo.pti_total_system);
+			getrusage(RUSAGE_SELF, &r);
+			tFin = 1000000*r.ru_utime.tv_sec +  r.ru_utime.tv_usec + 1000000*r.ru_stime.tv_sec +  r.ru_stime.tv_usec;;
+			timeToSleep = 400;
+			if(nb_of_timer%100000 == 1)
+			{
+				printf("temps passé sur 100us : %lu\n", tFin-tDepart );
+			}
+			break;
+		
+		default : 
+			break;
+	}
+	nb_of_timer++ ; 
 }
